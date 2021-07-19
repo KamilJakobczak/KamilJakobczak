16086818571079bd21b9ee589f47d5ff18e95ed9
KamilJakobczak <k_j1@vp.pl>
2021-07-19 11:05:41 +0200

    added functionality

--- index.js
+++ index.js
@@ -2,9 +2,32 @@ document.addEventListener('DOMContentLoaded', function () {
   console.log('Hello world');
 
   const rootElement = document.querySelector('#id');
-  const sections = document.querySelectorAll('section');
+  const sections = [...document.querySelectorAll('section')];
+  let currentSectionIndex = 0;
+  let isThrottled = false;
 
   document.addEventListener('wheel', function (e) {
-    console.log(e.wheelDelta);
+    if (isThrottled) return;
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+    }, 1000);
+    // console.log(e.wheelDelta);
+    const direction = e.wheelDelta < 0 ? 1 : -1;
+
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const isfirstSection = currentSectionIndex === 0;
+      if (isfirstSection) return;
+    }
+
+    currentSectionIndex += direction;
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
   });
 });
