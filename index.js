becbf3cd12fd5b36734a651d96fe1e8e8e7e1048
KamilJakobczak <k_j1@vp.pl>
2021-07-20 12:24:39 +0200

    Add support for keyboard

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('wheel', scroller.listenScroll);
   document.addEventListener('swipeUp', () => scroller.scroll(1));
   document.addEventListener('swipeDown', () => scroller.scroll(-1));
+  document.addEventListener('keydown', e => {
+    switch (e.keyCode) {
+      case 40:
+        return scroller.scroll(1);
+      case 38:
+        return scroller.scroll(-1);
+
+      default:
+        break;
+    }
+  });
 });
